d1169b87d8cb6ab33cf2c4e632f45ecfc5bb0001
oscararrise <oscar.jimenez@arrise.com>
2026-08-24 11:00:30 -0500

    Register sensor automation policies in admin

--- aplicaciones/dashboard/admin.py
+++ aplicaciones/dashboard/admin.py
@@ -1,1 +1,27 @@
-"""Dashboard configuration lives in aplicaciones.core.admin."""
+from django.contrib import admin
+
+from .models import SensorAutomationPolicy
+
+
+@admin.register(SensorAutomationPolicy)
+class SensorAutomationPolicyAdmin(admin.ModelAdmin):
+    list_display = (
+        "client",
+        "sensor_name",
+        "automation_level",
+        "is_enabled",
+        "requires_confirmation",
+        "email_enabled",
+        "whatsapp_enabled",
+        "updated_at",
+    )
+    list_filter = (
+        "client",
+        "automation_level",
+        "is_enabled",
+        "requires_confirmation",
+        "email_enabled",
+        "whatsapp_enabled",
+    )
+    search_fields = ("sensor_id", "sensor_name", "metric_name", "email_recipients")
+    readonly_fields = ("created_at", "updated_at")
