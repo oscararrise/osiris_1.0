5da2ad19c2e2c713aa552ec1a95c1dc44a0e1a2a
oscararrise <oscar.jimenez@arrise.com>
2026-09-05 12:13:17 -0500

    fix(sensor-config): sort migration imports

--- aplicaciones/sensor_config/migrations/0001_initial.py
+++ aplicaciones/sensor_config/migrations/0001_initial.py
@@ -1,9 +1,9 @@
 from decimal import Decimal
 
 import django.core.validators
+import django.db.models.deletion
 from django.conf import settings
 from django.db import migrations, models
-import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
