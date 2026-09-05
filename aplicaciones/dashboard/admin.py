c5830100493b2bd7910347ff9ba0cee518489dd4
oscararrise <oscar.jimenez@arrise.com>
2026-09-05 13:41:52 -0500

    feat(dashboard): expose agronomic relationships in admin

--- aplicaciones/dashboard/admin.py
+++ aplicaciones/dashboard/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import SensorAutomationPolicy
+from .models import AgronomicVariableRelationship, SensorAutomationPolicy
 
 
 @admin.register(SensorAutomationPolicy)
@@ -24,4 +24,26 @@ class SensorAutomationPolicyAdmin(admin.ModelAdmin):
         "whatsapp_enabled",
     )
     search_fields = ("sensor_id", "sensor_name", "metric_name", "email_recipients")
+    readonly_fields = ("created_at", "updated_at")
+
+
+@admin.register(AgronomicVariableRelationship)
+class AgronomicVariableRelationshipAdmin(admin.ModelAdmin):
+    list_display = (
+        "client",
+        "crop_name",
+        "sensor_name",
+        "name",
+        "relationship_type",
+        "is_enabled",
+        "updated_at",
+    )
+    list_filter = ("client", "relationship_type", "is_enabled")
+    search_fields = (
+        "crop_name",
+        "sensor_id",
+        "sensor_name",
+        "name",
+        "agronomic_goal",
+    )
     readonly_fields = ("created_at", "updated_at")
