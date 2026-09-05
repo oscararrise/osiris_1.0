4824bcfee45be5dce1913db2e1d629c13a5b662b
oscararrise <oscar.jimenez@arrise.com>
2026-09-05 13:05:17 -0500

    feat(sensor-config): route dashboard sensor toggle

--- aplicaciones/sensor_config/urls.py
+++ aplicaciones/sensor_config/urls.py
@@ -5,6 +5,11 @@ from . import views
 urlpatterns = [
     path("", views.sensor_configuration, name="sensor_configuration"),
     path(
+        "<int:sensor_pk>/dashboard/",
+        views.toggle_sensor_dashboard,
+        name="sensor_configuration_toggle",
+    ),
+    path(
         "<int:sensor_pk>/",
         views.sensor_configuration_detail,
         name="sensor_configuration_detail",
