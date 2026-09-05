55384fdef46d2c185bc1f1bd2d718e4955946691
oscararrise <oscar.jimenez@arrise.com>
2026-09-05 12:09:16 -0500

    feat(sensor-config): register app config

---
+++ aplicaciones/sensor_config/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class SensorConfigConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "aplicaciones.sensor_config"
+    verbose_name = "Configuración de sensores"
