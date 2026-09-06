71edf09a715dddb3c040a6f3cf098dab478a57c8
oscararrise <oscar.jimenez@arrise.com>
2026-09-05 21:20:06 -0500

    style(satellite): format test imports

--- aplicaciones/satellite/tests.py
+++ aplicaciones/satellite/tests.py
@@ -2,7 +2,11 @@ from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from aplicaciones.core.models import Client
-from aplicaciones.satellite.models import SatelliteField, SatelliteJob, SatelliteScene
+from aplicaciones.satellite.models import (
+    SatelliteField,
+    SatelliteJob,
+    SatelliteScene,
+)
 
 
 VALID_POLYGON = {
